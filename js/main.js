72c5a5c0c1dc78a183cffb09a768557b8158a56e
Egor <work4egor@gmail.com>
2015-04-15 18:43:40 +0300

    add sass, add fancy box and some fixes

--- js/main.js
+++ js/main.js
@@ -1,27 +1,28 @@
 $(document).ready(function() {
-            $('.jcarousel').on('jcarousel:create jcarousel:reload', function() {
-                var element = $(this),
-                    width = element.innerWidth();
+	$('.jcarousel').on('jcarousel:create jcarousel:reload', function() {
+		var element = $(this),
+			width = element.innerWidth();
 
-                // This shows 1 item at a time.
-                // Divide `width` to the number of items you want to display,
-                // eg. `width = width / 3` to display 3 items at a time.
-                element.jcarousel('items').css('width', width + 'px');
-            })
-            .jcarousel({
-                wrap:'circular'
-            })
-            .jcarouselAutoscroll({
-                interval: 4000,
-                target: '+=1',
-                autostart: true
-            });
+		// This shows 1 item at a time.
+		// Divide `width` to the number of items you want to display,
+		// eg. `width = width / 3` to display 3 items at a time.
+		element.jcarousel('items').css('width', width + 'px');
+	})
+	.jcarousel({
+		wrap:'circular'
+	})
+	.jcarouselAutoscroll({
+		interval: 4000,
+		target: '+=1',
+		autostart: true
+	});
 
-            $('.jcarousel-prev').jcarouselControl({
-                target: '-=1'
-            });
+	$('.jcarousel-prev').jcarouselControl({
+		target: '-=1'
+	});
 
-            $('.jcarousel-next').jcarouselControl({
-                target: '+=1'
-            });
+	$('.jcarousel-next').jcarouselControl({
+		target: '+=1'
+	});
+	$('.fancybox').fancybox();		
 });
